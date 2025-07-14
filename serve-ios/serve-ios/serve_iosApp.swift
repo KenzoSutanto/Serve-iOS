27bfe94465d8e77c4f670d4aaab3bd3d14fabdd2
KenzoSutanto <kenzosutanto0406@gmail.com>
2025-07-14 16:11:54 +0800

    code

---
+++ serve-ios/serve-ios/serve_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  serve_iosApp.swift
+//  serve-ios
+//
+//  Created by Kenzo on 14/7/25.
+//
+
+import SwiftUI
+
+@main
+struct serve_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ serve-ios/serve-iosTests/serve_iosTests.swift
@@ -0,0 +1,16 @@
+//
+//  serve_iosTests.swift
+//  serve-iosTests
+//
+//  Created by Kenzo on 14/7/25.
+//
+
+import Testing
+
+struct serve_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
