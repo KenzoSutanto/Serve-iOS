5a6249e7936b1a276394dfc65f0ef327b26b1dcf
KenzoSutanto <kenzosutanto0406@gmail.com>
2025-07-14 16:13:12 +0800

    more code

---
+++ serve-ios/serve-ios/game.swift
@@ -0,0 +1,18 @@
+//
+//  game.swift
+//  serve-ios
+//
+//  Created by Kenzo on 14/7/25.
+//
+
+import SwiftUI
+
+struct game: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    game()
+}
---
+++ serve-ios/serve-ios/settings.swift
@@ -0,0 +1,18 @@
+//
+//  settings.swift
+//  serve-ios
+//
+//  Created by Kenzo on 14/7/25.
+//
+
+import SwiftUI
+
+struct settings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    settings()
+}
